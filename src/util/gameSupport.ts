5934fc1899dce59c6b37c73246970873f9623210
TanninOne <seppleviathan@gmx.de>
2021-10-07 17:07:50 +0200

    avoid using remote

--- src/util/gameSupport.ts
+++ src/util/gameSupport.ts
@@ -1,9 +1,6 @@
-import { app as appIn, remote } from 'electron';
 import * as path from 'path';
 import * as Redux from 'redux';
-import { types } from 'vortex-api';
-
-const app = appIn || remote.app;
+import { types, util } from 'vortex-api';
 
 interface IGameSupport {
   fileFilter?: (fileName: string) => boolean;
@@ -213,7 +210,7 @@ export function bsaVersion(gameId: string): number {
 }
 
 export function mygamesPath(gameMode: string): string {
-  return path.join(app.getPath('documents'), 'My Games',
+  return path.join(util.getVortexPath('documents'), 'My Games',
                    gameSupport[gameMode].mygamesPath);
 }
 
