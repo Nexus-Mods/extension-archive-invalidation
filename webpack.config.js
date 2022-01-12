a86dcb446bdf313b888412c49e8efcd4f4bfa043
TanninOne <seppleviathan@gmx.de>
2022-01-12 14:38:37 +0100

    updated dependencies/webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,3 @@
 let webpack = require('vortex-api/bin/webpack').default;
 
-module.exports = webpack('gamebryo-archive-invalidation', __dirname, 4);
+module.exports = webpack('gamebryo-archive-invalidation', __dirname, 5);
